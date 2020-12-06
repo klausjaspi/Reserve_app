3316ade6647c4b9ab19ab934710d5fa8e4aae4bc
klausjaspi <klaus.jaspi@gmail.com>
2020-12-06 19:23:06 +0200

    new inputs

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
  document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
     this.querySelector('.custom-select').classList.toggle('open');
     this.querySelector('.anotherone').classList.toggle('open');
-    this.querySelector('.calendarbar').classList.toggle('open');
-    this.querySelector('.calendarbar2').classList.toggle('open');
  })
 
 
@@ -12,6 +10,16 @@
         this.querySelector('.custom-select').classList.toggle('open');
     })
 }*/
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
 
  window.addEventListener('click', function (e) {
      const select = document.querySelector('.custom-select')
@@ -22,22 +30,6 @@
      if (!select2.contains(e.target)) {
         select2.classList.remove('open');
     }
-    const select3 = document.querySelector('.calendarbar')
-     if (!select3.contains(e.target)) {
-        select3.classList.remove('open');
-    }
-    const select4 = document.querySelector('.calendarbar2')
-     if (!select3.contains(e.target)) {
-        select4.classList.remove('open');
-    }
  });
 
-
-/*window.addEventListener('click', function(e) {
-    for (const select of document.querySelectorAll('.custom-select')) {
-        if (!select.contains(e.target)) {
-            select.classList.remove('open');
-        }
-    }
-});*/
-
+ 
